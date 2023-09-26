265f420c2cbc3ac3c9a6e97d4a7accc4e71ee7ad
piterstyle <146103400+piterstyle@users.noreply.github.com>
2023-09-26 17:15:48 +0300

    upload

---
+++ data.js
@@ -0,0 +1,106 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-q1",
+      "name": "q1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6764523798998994,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-q2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-q2",
+      "name": "q2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.437398833907352,
+          "pitch": 0.007340399536822417,
+          "rotation": 0,
+          "target": "0-q1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-q3",
+      "name": "q3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
